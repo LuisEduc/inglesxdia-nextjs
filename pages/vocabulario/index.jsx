897596004f1ba64673eb3edfd0bb0a69254fdd43
luiis <luiis.educa@gmail.com>
2022-02-07 03:29:28 -0500

    ads-mob_bien marginTop full-voc

--- pages/vocabulario/index.jsx
+++ pages/vocabulario/index.jsx
@@ -31,7 +31,7 @@ export default function index({ data }) {
                 // full-voc
                 client='ca-pub-3630578707238850'
                 slot='1345454840'
-                style={{ display: 'block', textAlign: 'center' }}
+                style={{ display: 'block', textAlign: 'center', marginTop: 30+'px' }}
                 format='auto'
                 responsive='true'
                 layoutKey='-gw-1+2a-9x+5c'
