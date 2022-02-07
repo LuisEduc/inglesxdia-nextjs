280542d90c0e8c152e3a05b627f5606aca9e5471
luiis <luiis.educa@gmail.com>
2022-02-07 02:29:27 -0500

    usa react-adsense

--- pages/lecs/[categoria]/[slug].jsx
+++ pages/lecs/[categoria]/[slug].jsx
@@ -12,7 +12,7 @@ import "react-h5-audio-player/lib/styles.css";
 import Image from "next/image"
 import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
-import AdSense from 'react-ssr-adsense';
+import AdSense from 'react-adsense';
 
 const settings = {
     showIndicators: false,
