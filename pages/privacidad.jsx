228e70b153e189ecf3c0fb5e69e0096f1264ff83
luiis <luiis.educa@gmail.com>
2022-02-13 18:48:53 -0500

    privacidad v1

--- pages/privacidad.jsx
+++ pages/privacidad.jsx
@@ -1,5 +1,6 @@
 import Layout from "../components/Layout"
 import Head from "next/head"
+import Link from "next/link"
 
 export default function privacidad() {
 
@@ -13,112 +14,30 @@ export default function privacidad() {
                 <h2 className="mt-3">Política de Privacidad</h2>
                 <p>Esta política de privacidad explica nuestra práctica a la hora de recopilar, tratar y
                     divulgar nuestros datos. En ella se recoge el procedimiento mediante el cual gestionamos la
-                    información de carácter personal que el servicio inglesxdia.tech y/o su app, en
+                    información de carácter personal que el servicio inglesxdia.tech y su app, en
                     adelante INGLESXDIA, recopila y recibe de ti.</p>
                 <p>INGLESXDIA presta su servicio de acuerdo con los términos y condiciones de servicio y esta
-                    política de privacidad.</p>
-                <p><br /><strong>INFORMACIÓN QUE RECOGEMOS SOBRE TI Y CÓMO LA USAMOS</strong></p>
-                <p>Cuando creas una cuenta, INGLESXDIA recoge información de carácter personal como tu
-                    dirección de correo electrónico, fecha de nacimiento, sexo, foto de perfil, así
-                    como aquellas preferencias que introduzcas en el Servicio como el idioma.</p>
-                <p>INGLESXDIA utilizará tu información personal para los siguientes fines generales:</p>
-                <p>(a) prestarte el Servicio.<br />(b) responder a tus preguntas y comentarios.<br />(c) contactar
-                    contigo a través del correo electrónico (o a través de otros canales
-                    electrónicos) para informarte sobre el Servicio.<br />(d) con fines
-                    estadísticos<br />(e) con cualquier otro fin sobre el cual se te haya informado en el momento
-                    de la recogida de datos.<br />Cuando utilizas el Servicio, puedes crear tu perfil personal,
-                    establecer contactos, enviar mensajes, llevar a cabo búsquedas, resolver dudas o utilizar
-                    nuestro chat para conversar con otros usuarios. INGLESXDIA recopila y guarda dicha
-                    información con el fin de prestarte el Servicio y ofrecerte funciones personalizadas.
-                    INGLESXDIA también recibe y registra información procedente de tu ordenador y
-                    navegador, incluida tu dirección IP, información de cookies; características
-                    del software y hardware e información sobre las páginas que solicitas,
-                    independientemente de si creas una cuenta o no.</p>
-                <p><br /><strong>ALMACENAMIENTO DE LA INFORMACIÓN</strong></p>
-                <p>Guardamos tu información de carácter personal únicamente en servidores seguros
-                    dentro de instalaciones seguras de la Unión Europea y con la protección de firewall
-                    actualizada. Los datos que nos proporcionas pueden transferirse, almacenarse y/o procesarse por
-                    parte de nuestro personal o el personal de uno de nuestros proveedores. Al proporcionarnos tus datos
-                    personales, aceptas que se lleve a cabo su transferencia, almacenamiento o procesamiento.
-                    Adoptaremos todas las medidas razonablemente necesarias para asegurar que tus datos se traten de
-                    modo seguro y conforme a la Política de Privacidad.</p>
+                    política de privacidad. Su privacidad es importante para nosotros. Para proteger mejor su privacidad, le proporcionamos este aviso que explica nuestras prácticas de información online y las opciones que puede elegir sobre la forma en que se recoge y utiliza su información. 
+</p>
+                <p><br /><strong>DATOS ANÓNIMOS QUE RECOGEMOS</strong></p>
+                <p>Utilizamos información no identificable y agregada para hacer un seguimiento de los patrones de uso y tráfico para poder diseñar mejor nuestra web/app. Cuando usted visita este sitio, cierto tipo de información no personal, como su dirección IP, cualquier sitio web que le haya remitido a nosotros, su tipo de navegador e idioma, y las horas de acceso, pueden ser recogidos automáticamente como parte del funcionamiento del sitio. También podemos recopilar información sobre la navegación, incluyendo información sobre las páginas que ve, los enlaces en los que hace clic y otras acciones realizadas en relación con el sitio. Esta información es utilizada por un tercero (Google Analytics) para proporcionarnos estadísticas sobre el uso del sitio. Google Analytics sólo tiene acceso a datos totalmente anónimos y no comparte estos datos con ningún otro tercero. </p>
+                <p><br /><strong>LO QUE NO RECOGEMOS</strong></p>
+                <p>No recopilamos ninguna información personal identificable (PII) sobre usted a menos que usted nos envíe voluntariamente dicha información, por ejemplo, solicitándonos información o enviándonos un correo electrónico. No vendemos, alquilamos, cedemos ni compartimos información sobre usted con terceros. Las cookies se utilizan en esta web/app principalmente para analizar el tráfico de nuestra web/a, para mostrar anuncios o para medir los índices de éxito de la publicidad.</p>
                 <p><br /><strong>COOKIES</strong></p>
-                <p>Las cookies son archivos de texto que se colocan en su computadora para recopilar información
-                    de registro de Internet estándar e información de comportamiento del visitante. Esta
-                    información se usa para rastrear el uso de visitantes del sitio web/app y para compilar informes
-                    estadísticos sobre la actividad del sitio web/app. Para obtener más información,
-                    visite www.aboutcookies.org o www.allaboutcookies.org. Puede configurar su navegador para que no
-                    acepte cookies y los sitios web/app anteriores le indican cómo eliminar las cookies de su
-                    navegador. Sin embargo, en algunos casos, algunas de las funciones de nuestro sitio web/app pueden no
-                    funcionar como resultado.</p>
-                <p><br /><strong>DIVULGACIÓN DE INFORMACIÓN</strong></p>
-                <p>INGLESXDIA divulga la información de tu perfil (salvo tu dirección de correo
-                    electrónico y contraseña) a otros usuarios que buscan estudiantes para conversar en
-                    nuestra sala de chat y para otras funciones del sitio.</p>
-                <p>INGLESXDIA no alquilará, venderá ni compartirá tu información de
-                    carácter personal con terceros, excepto para poder proporcionarte los productos y servicios
-                    solicitados por ti, siempre y cuando tengamos tu autorización o en las siguientes
-                    circunstancias:</p>
-                <p>cuando INGLESXDIA deba atender a una citación judicial, resolución de un juez o
-                    tribunal o a un procedimiento judicial, o deba establecer o ejercer los derechos jurídicos
-                    que le correspondan o defenderse contra demandas judiciales;<br />cuando INGLESXDIA considere
-                    necesario compartir información con el fin de investigar, prevenir o reaccionar ante
-                    actividades ilegales, sospechas de fraude, posibles amenazas a la seguridad física de
-                    cualquier persona, infracciones de los Términos y condiciones del servicio o en cualquier
-                    otro caso en el que la ley lo exija;<br />cuando INGLESXDIA transfiera tu información en caso
-                    de que INGLESXDIA sea adquirida o se fusione con otra empresa. Si así fuera, INGLESXDIA te lo
-                    comunicará antes de que la información se transfiera y pase a estar sujeta a una
-                    política de privacidad diferente.<br />Recuerda que la información que divulgues
-                    online de forma voluntaria puede recopilarse y utilizarse por parte de un tercero. La
-                    información publicada online es accesible públicamente, por lo que podrías
-                    recibir mensajes no solicitados procedentes de un tercero.</p>
-                <p><br /><strong>ANUNCIOS DE TERCEROS</strong></p>
-                <p>INGLESXDIA muestra anuncios personalizados basados en información de carácter personal.
-                    Los anunciantes pueden considerar que las personas que interaccionan con dichos anuncios, los
-                    visualizan o hacen clic en ellos cumplen con los requisitos de su público objetivo.</p>
-                <p>INGLESXDIA no proporciona información de carácter personal al anunciante cuando
-                    interactúas con el anuncio o lo visualizas. No obstante, por el solo hecho de interactuar o
-                    visualizar el anuncio, darás tu consentimiento para que el anunciante interprete que te
-                    hallas entre su público objetivo. De igual modo, nosotros podríamos ofrecerte anuncios
-                    personalizados en nombre de uno o varios terceros, aunque sin compartir en ningún caso tu
-                    dirección de correo electrónico ni otra información con tales terceros.</p>
-                <p><br />INGLESXDIA muestra anuncios de la red publicitaria de Google Adsense en su sitio web/app y de
-                    Google Admob en su app. En el siguiente enlace puedes econtrar información de cómo se
-                    administran los datos en los productos publicitarios de Google:
-                    https://policies.google.com/technologies/partner-sites</p>
-                <p><br /><strong>CONFIDENCIALIDAD Y SEGURIDAD</strong></p>
-                <p>Adoptaremos medidas para proteger los datos personales mediante la aplicación de medidas de
-                    seguridad razonables propias del sector frente a pérdidas o robo, así como acceso no
-                    autorizado, divulgación, copia, uso inadecuado o modificación de los mismos. Tus datos
-                    personales están protegidos con contraseña y, salvo en los casos previstos en la
-                    Política de Privacidad, sólo tú y nosotros podemos visualizarlos. Tú
-                    aceptas preservar la confidencialidad de tu/s contraseña/s y a no divulgarla/s a nadie
-                    más ni permitir que se divulguen.</p>
-                <p>INGLESXDIA limitará el acceso a tu información de carácter personal a aquellos
-                    empleados que, según los criterios razonables de INGLESXDIA, precisen acceso a dicha
-                    información para proporcionarte productos o prestarte el Servicio, o con objeto de
-                    desempeñar sus funciones.</p>
-                <p>INGLESXDIA ha implementado medidas y procedimientos de seguridad, físicos, electrónicos
-                    y lógicos, conforme a la legislación relativa a la protección de la
-                    información de carácter personal. No obstante, debes tener en cuenta que la seguridad
-                    de las transmisiones de datos en internet no puede garantizarse al 100 %. Así pues,
-                    INGLESXDIA no puede asegurarte ni garantizarte la seguridad de cualquier información que nos
-                    envíes. Concretamente, el acceso al Servicio y el uso de este por tu parte corre
-                    únicamente a tu cuenta y riesgo y tú serás el responsable de restringir el
-                    acceso a tu ordenador y de asegurarte de que esté completamente limpio de todo tipo de
-                    códigos maliciosos, spyware, virus, troyanos, etcétera, que puedan rastrear los datos
-                    que introduces en el Servicio, incluida la dirección de correo electrónico y la
-                    información relativa a pagos. No asumimos responsabilidad alguna por las pérdidas o
-                    perjuicios derivados del incumplimiento de este párrafo por parte del usuario.</p>
-                <p><br /><strong>SITIOS WEB DE TERCEROS</strong></p>
-                <p>Nuestro sitio web contiene enlaces a otros sitios web. Esta política de privacidad solo se
-                    aplica a este sitio web, por lo que cuando accedas a otro sitio web desde un enlace, debes leer sus
-                    propias políticas de privacidad.</p>
-                <p><br /><strong>ACCEDER A TUS DATOS</strong></p>
-                <p>Puedes obtener una copia de la información de carácter personal que INGLESXDIA tiene
-                    accediendo a tu perfil desde el Sitio web o App o también puedes solicitar que INGLESXDIA
-                    actualice, modifique o elimine dicha información escribiendo al correo
-                    contacto@inglesxdia.tech.</p>
+                <p>Nuestra web/app utiliza las llamadas cookies. Las cookies son pequeños archivos que los sitios web colocan en su ordenador mientras los visita. Los archivos contienen información específica relacionada con el usuario. Las cookies nos permiten saber con qué frecuencia y cuántas personas utilizan nuestra web/app y ofrecer servicios de forma más personalizada. Utilizamos cookies de sesión, que almacenan temporalmente información mientras dura su visita nuestra web/app. Las cookies de sesión se eliminan automáticamente al final de su visita. También utilizamos cookies persistentes para almacenar información sobre los usuarios que visitan con frecuencia uno o varios de nuestros sitios. El propósito de utilizar estas cookies es proporcionarle una orientación óptima al usuario y también ayudarnos a reconocerle cuando vuelva, para poder ofrecerle la máxima variedad y nuevos contenidos. El contenido de una cookie permanente se limita a un número de identificación. No se almacenan nombres, direcciones IP, etc., ni construimos perfiles individuales de su comportamiento de uso. Es posible utilizar nuestros servicios sin cookies. También puede eliminar las cookies del disco duro de su ordenador en cualquier momento. En ese caso, sin embargo, es posible que no pueda beneficiarse de todo el potencial de las funciones de los sitios web. Puede encontrar más información en 
+                <Link href={'https://inglesxdia.tech/cookies'}>
+                <a> Cookies</a>
+                </Link>.</p>
+                <p><br /><strong>POLÍTICA DE PRIVACIDAD PARA EL USO DE GOOGLE ANALYTICS</strong></p>
+                <p>Esta web/app utiliza Google Analytics, un servicio de análisis web proporcionado por Google, Inc. ("Google"). Google Analytics utiliza "cookies", que son archivos de texto ubicados en su ordenador, para ayudar a la web/app a analizar el uso que usted hace del mismo. Google utilizará esta información con el fin de evaluar su uso de la web/app para elaborar informes sobre la actividad de la web/app para el operador de la web/app y prestar otros servicios relacionados con la actividad de la web/app y el uso de Internet. La dirección IP transmitida por su navegador en el marco de Google Analytics no se combinará con otros datos de Google. Puede evitar el almacenamiento de cookies seleccionando la configuración adecuada en su navegador; sin embargo, tenga en cuenta que si lo hace, es posible que no pueda utilizar todas las funciones de esta web/app. Además, puede evitar que los datos capturados por la cookie y relacionados con su uso de la web/app (incluida su dirección IP) sean capturados y procesados por Google descargando e instalando el complemento del navegador disponible en el siguiente enlace: <a className="text-info font-weight-bold" onClick={()=> window.open("https://chrome.google.com/webstore/detail/google-analytics-opt-out/fllaojicojecljbmefodhfapmkghcbnh?hl=en", "_blank")}  
+                    >https://chrome.google.com/webstore/detail/google-analytics-opt-out/fllaojicojecljbmefodhfapmkghcbnh?hl=en</a>. Puede encontrar más información sobre el uso de los datos y la política de privacidad de Google Analytics en <a className="text-info font-weight-bold" onClick={()=> window.open("https://www.google.com/analytics/terms/us.html", "_blank")}  
+                    >https://www.google.com/analytics/terms/us.html</a></p>
+                <p><br /><strong>POLÍTICA DE PRIVACIDAD PARA EL USO DE GOOGLE ADSENSE (ADMOB)</strong></p>
+                <p>Esta web/app utiliza Google AdSense para mostrar publicidad. El proveedor de estos servicios es Google Inc., 1600 Amphitheatre Parkway Mountain View, CA 94043, USA. Google Adsense utiliza las llamadas "cookies", archivos de texto que se almacenan en su ordenador para analizar el uso de la web/app. Además, Google AdSense también utiliza los llamados web beacons (imágenes invisibles). Estos web beacons ayudan a procesar información, como el tráfico de visitantes en esta web/app. La información generada por las cookies y las balizas web sobre su uso de esta web/app (incluida su dirección IP) y el envío de formatos publicitarios se envía y se almacena en un servidor de Google en EE.UU. Google también puede transferir esta información a sus socios contractuales. Sin embargo, Google no asociará su dirección IP con ningún otro dato almacenado en Google. Puede evitar la instalación de cookies ajustando la configuración de su navegador; sin embargo, si lo hace, es posible que no pueda utilizar todas las funciones de esta web/app. Al utilizar esta web/app, usted acepta el tratamiento de la información recopilada por parte de Google, de acuerdo con la finalidad mencionada anteriormente. Puede obtener más información sobre la política de privacidad de Google Adsense en: <a className="text-info font-weight-bold" onClick={()=> window.open("https://policies.google.com/technologies/ads", "_blank")}  
+                    >https://policies.google.com/technologies/ads</a>. Puede inhabilitar la personalización de los anuncios en: <a className="text-info font-weight-bold" onClick={()=> window.open("https://adssettings.google.com", "_blank")}  
+                    >https://adssettings.google.com</a></p>
+                <p><br /><strong>CAMBIOS EN NUESTRA POLÍTICA DE PRIVACIDAD</strong></p>
+                <p>Puede revisar esta política de privacidad en cualquier momento. Debería visitar esta página periódicamente para revisar nuestra Política de Privacidad actual, de modo que siempre sepa qué información recopilamos y cómo podemos utilizarla.</p>
             </main>
         </Layout>
     )
