d31d718bdda125735405e54cd932c79fb4d9a7b0
luiis <luiis.educa@gmail.com>
2022-03-17 11:08:32 -0500

    inline-block full-voc v4

--- pages/vocabulario/index.jsx
+++ pages/vocabulario/index.jsx
@@ -31,8 +31,10 @@ export default function index({ data }) {
                 // full-voc
                 client='ca-pub-3630578707238850'
                 slot='1345454840'
-                style={{ display: 'inline-block', marginTop: 30 + 'px' }}
+                style={{ display: 'inline-block', textAlign: 'center', marginTop: 30 + 'px', height: 100 +'%' }}
+                format='auto'
                 responsive='true'
+                layoutKey='-gw-1+2a-9x+5c'
             />
 
             <div className="grid-info">
