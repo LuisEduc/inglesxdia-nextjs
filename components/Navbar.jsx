85f7964feca240a9754645026750ca70f2d3b3c7
luiis <luiis.educa@gmail.com>
2022-03-17 14:55:10 -0500

    no banner descarga

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -51,12 +51,12 @@ export default function Navbar() {
                         </div>
                     </div>
                 </div>
-                <div className="descarga-app" onClick={() => window.open("https://bit.ly/34j0kVS", "_blank")}>
+                {/* <div className="descarga-app" onClick={() => window.open("https://bit.ly/34j0kVS", "_blank")}>
                     <span>Descargar aplicación gratis</span>
                     <img src="/play.svg" />
-                </div>
+                </div> */}
             </div>
-            <div style={{ height: 110 }}></div>
+            <div style={{ height: 80 }}></div>
         </>
     )
 }
