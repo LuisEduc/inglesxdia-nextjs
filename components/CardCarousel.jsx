08a499100dc9e22f4c0807abbf3affb306932631
luiis <luiis.educa@gmail.com>
2022-03-25 09:53:46 -0500

    300x90-voc 100%

--- components/CardCarousel.jsx
+++ components/CardCarousel.jsx
@@ -65,18 +65,9 @@ export default function CardCarousel({ data }) {
         client='ca-pub-3630578707238850'
         slot='4436723862'
         className='ads-mob'
-        style={{ display: 'block', height: 90 + 'px', marginBottom: 15 + 'px', marginTop: -8 + 'px' }}
+        style={{ display: 'block', width: 100 + '%', height: 90 + 'px', marginBottom: 15 + 'px', marginTop: -8 + 'px' }}
         format=''
-        responsive='true'
       />
-      {/* <AdSense.Google
-        // 300x250-voc
-        client='ca-pub-3630578707238850'
-        slot='6102898405'
-        className='ads-mob'
-        style={{ display: 'inline-block', width: 300 + 'px', height: 250 + 'px', margin: '0 auto 30px' }}
-        format=''
-      /> */}
       <div className="audio-palabras mx-auto">
         <AudioPlayer
           ref={refAudio}
