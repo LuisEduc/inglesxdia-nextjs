dd6c594b56b9bda2a9babb0a53a2fd11a0e0d427
luiis <luiis.educa@gmail.com>
2022-03-30 08:57:03 -0500

    banner-inferior-fijo ads-mob

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,6 +12,7 @@ export default function Footer() {
                 <AdSense.Google
                     // banner-inferior-fijo
                     client='ca-pub-3630578707238850'
+                    className='ads-mob'
                     slot='2949297043'
                     style={{ display: 'block', height: 50 + 'px', width: 80 + '%', marginLeft: 'auto', marginRight: 'auto' }}
                     format=''
