96a47dcf76bc24a9592d584e9a5355e4b1cd8b2b
luiis <luiis.educa@gmail.com>
2022-03-30 09:00:19 -0500

    banner-inferior-fijo maxHeight-min

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,9 +12,8 @@ export default function Footer() {
                 <AdSense.Google
                     // banner-inferior-fijo
                     client='ca-pub-3630578707238850'
-                    className='ads-mob'
                     slot='2949297043'
-                    style={{ display: 'block', height: 50 + 'px', width: 80 + '%', marginLeft: 'auto', marginRight: 'auto' }}
+                    style={{ display: 'block', maxHeight: 90 + 'px', minHeight: 50 + 'px', width: 80 + '%', marginLeft: 'auto', marginRight: 'auto' }}
                     format=''
                 />
             </div>
