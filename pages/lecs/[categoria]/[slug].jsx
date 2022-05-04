92fea941145737c22031b4f68cadb9832ae4e4c9
luiis <luiis.educa@gmail.com>
2022-05-03 19:33:49 -0500

    300x90-indi-alto mob desk

--- pages/lecs/[categoria]/[slug].jsx
+++ pages/lecs/[categoria]/[slug].jsx
@@ -77,7 +77,6 @@ export default function Individual({ dataLec, dataCat }) {
                     // 300x90-indi-alto
                     client='ca-pub-3630578707238850'
                     slot='7109410627'
-                    className='ads-mob'
                     style={{
                         display: 'block',
                         height: 90 + 'px',
