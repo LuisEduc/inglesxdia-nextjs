7e5be836476d0dbd2acc30e180c0b86f14665196
luiis <luiis.educa@gmail.com>
2022-05-06 12:06:35 -0500

    bootstrap.min.js Script app.js

--- pages/_app.js
+++ pages/_app.js
@@ -38,13 +38,20 @@ const App = ({ Component, pageProps }) => {
   return (
     <>
       {/* Global Site Tag (gtag.js) - Google Analytics */}
-      
+
 
       {/* <Script
         src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
         strategy="afterInteractive"
         async=""
       /> */}
+
+      <Script
+        strategy="afterInteractive"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js"
+        integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc"
+        crossOrigin="anonymous">
+      </Script>
 
       <Script
         strategy="afterInteractive"
