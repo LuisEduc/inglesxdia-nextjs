3784940c0d8949576e2f328aa053109ef8404cdc
luiis <luiis.educa@gmail.com>
2022-05-20 15:15:24 -0500

    sin full v10

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,77 +6,79 @@ import Link from "next/link";
 export default function Hero({ buscar }) {
     return (
         <>
-            <AdSense.Google
-                // 300x100-inicio-hero
-                client='ca-pub-3630578707238850'
-                slot='8009250674'
-                style={{
-                    display: 'block',
-                    height: 100 + 'px',
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                    textAlign: 'center'
-                }}
-                format=''
-                responsive='true'
-            />
-            <div className="hero hero-bg">
-                <div className="hero-text">
-                    <div className="hero-titulo">
-                        <h1>Estudiar inglés online<span className="text-primary">.</span></h1>
+            <div className="hero-bg">
+                <AdSense.Google
+                    // 300x100-inicio-hero
+                    client='ca-pub-3630578707238850'
+                    slot='8009250674'
+                    style={{
+                        display: 'block',
+                        height: 100 + 'px',
+                        marginLeft: 'auto',
+                        marginRight: 'auto',
+                        textAlign: 'center'
+                    }}
+                    format=''
+                    responsive='true'
+                />
+                <div className="hero">
+                    <div className="hero-text">
+                        <div className="hero-titulo">
+                            <h1>Estudiar inglés online<span className="text-primary">.</span></h1>
+                        </div>
+                        <div className="buscador">
+                            <Buscador buscar={buscar} />
+                        </div>
                     </div>
-                    <div className="buscador">
-                        <Buscador buscar={buscar} />
-                    </div>
-                </div>
 
-                <div className="hero-resumen-grid">
-                    <div className="hero-resumen-titulo">
-                        <h2>¿Qué ofrece <span className="resalt-mor">inglesxdia.tech</span>?</h2>
-                    </div>
-                    <div className="hero-resumen-item">
-                        <Link href="/vocabulario">
-                            <a>
-                                <div className="item-hero-number">
-                                    <span>1</span>
-                                </div>
-                            </a>
-                        </Link>
-                        <div className="item-hero-text">
+                    <div className="hero-resumen-grid">
+                        <div className="hero-resumen-titulo">
+                            <h2>¿Qué ofrece <span className="resalt-mor">inglesxdia.tech</span>?</h2>
+                        </div>
+                        <div className="hero-resumen-item">
                             <Link href="/vocabulario">
-                                <a>Vocabulario diario en inglés.</a>
+                                <a>
+                                    <div className="item-hero-number">
+                                        <span>1</span>
+                                    </div>
+                                </a>
                             </Link>
-                            <p>Vocabulario fácil, medio y avanzado en inglés. Significado, traducción, frases de ejemplo y pronunciación, todos los días.</p>
+                            <div className="item-hero-text">
+                                <Link href="/vocabulario">
+                                    <a>Vocabulario diario en inglés.</a>
+                                </Link>
+                                <p>Vocabulario fácil, medio y avanzado en inglés. Significado, traducción, frases de ejemplo y pronunciación, todos los días.</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="hero-resumen-item">
-                        <Link href="/#lecs">
-                            <a href="/#lecs">
-                                <div className="item-hero-number">
-                                    <span>2</span>
-                                </div>
-                            </a>
-                        </Link>
-                        <div className="item-hero-text">
+                        <div className="hero-resumen-item">
                             <Link href="/#lecs">
-                                <a href="/#lecs">Frases y expresiones.</a>
+                                <a href="/#lecs">
+                                    <div className="item-hero-number">
+                                        <span>2</span>
+                                    </div>
+                                </a>
                             </Link>
-                            <p>Expresiones y frases útiles en inglés, verbos con todas las letras, lecturas, vocabulario básico de inglés y mucho más.</p>
+                            <div className="item-hero-text">
+                                <Link href="/#lecs">
+                                    <a href="/#lecs">Frases y expresiones.</a>
+                                </Link>
+                                <p>Expresiones y frases útiles en inglés, verbos con todas las letras, lecturas, vocabulario básico de inglés y mucho más.</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="hero-resumen-item">
-                        <Link href="/#lecs">
-                            <a href="/#lecs">
-                                <div className="item-hero-number">
-                                    <span>3</span>
-                                </div>
-                            </a>
-                        </Link>
-                        <div className="item-hero-text" >
+                        <div className="hero-resumen-item">
                             <Link href="/#lecs">
-                                <a href="/#lecs">Ejercicios para estudiar.</a>
+                                <a href="/#lecs">
+                                    <div className="item-hero-number">
+                                        <span>3</span>
+                                    </div>
+                                </a>
                             </Link>
-                            <p>Todas las lecciones incluyen cuestionario, traducción y pronunciación en inglés-español, la voz es natural y pausada.</p>
+                            <div className="item-hero-text" >
+                                <Link href="/#lecs">
+                                    <a href="/#lecs">Ejercicios para estudiar.</a>
+                                </Link>
+                                <p>Todas las lecciones incluyen cuestionario, traducción y pronunciación en inglés-español, la voz es natural y pausada.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
