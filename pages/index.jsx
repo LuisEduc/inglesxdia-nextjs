7e8f96d55ff6d4f7e5728a796f49828dc6d79c98
luiis <luiis.educa@gmail.com>
2022-06-06 00:15:40 -0500

    title

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ export default function index({ bloques, cats, buscar }) {
         <Layout home buscar={buscar}>
             <Head>
                 <link rel="icon" href="/favicon.png" />
-                <title>Curso de inglés en línea | inglesxdia</title>
+                <title>Curso de inglés en línea / inglesxdia</title>
                 <meta name="description" content="Clases de inglés todos los días. Aprende a hablar, leer y escribir en inglés con nuestras lecciones diarias. Lecturas cortas, vocabulario, verbos y más clases con preguntas para practicar." />
             </Head>
 
