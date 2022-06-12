f7a9848f5a48f4966d282ef250f911991d2de463
luiis <luiis.educa@gmail.com>
2022-06-11 23:17:59 -0500

    isr nextjs

--- pages/lecs/[categoria]/[slug].jsx
+++ pages/lecs/[categoria]/[slug].jsx
@@ -350,6 +350,7 @@ export async function getStaticPaths() {
         }))
         return {
             paths,
+            // fallback: 'blocking',
             fallback: false,
         }
     } catch (error) {
