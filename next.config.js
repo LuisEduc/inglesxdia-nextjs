93c6800628294c02d763f90069ead78fd7044d00
luiis <luiis.educa@gmail.com>
2022-06-17 22:52:00 -0500

    smilewanted

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
+        source: "/",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "https://prebid.smilewanted.com" },
