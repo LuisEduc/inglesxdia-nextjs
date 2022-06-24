0d3bc5eb55e6cf552bc970565981db0a3307f734
luiis <luiis.educa@gmail.com>
2022-06-24 11:52:55 -0500

    adsense

--- pages/vocabulario/index.jsx
+++ pages/vocabulario/index.jsx
@@ -26,8 +26,8 @@ export default function IndexVoc({ data }) {
                     ezstandalone.display()
                 }
             } else {
-                ezstandalone.setEzoicAnchorAd(false)
                 setAdsenseActive(true)
+                ezstandalone.setEzoicAnchorAd(false)
                 console.log("adsenseActive true")
             }
         });
