882678d7a4681a7125828e7b1bf1f98db16b6b3d
luiis <luiis.educa@gmail.com>
2022-06-27 18:45:10 -0500

    ScriptAdSenseEzoic

--- components/ScriptAdSenseEzoic.jsx
+++ components/ScriptAdSenseEzoic.jsx
@@ -6,20 +6,16 @@ export default function ScriptAdSenseEzoic({ adsenseActive }) {
         <>
             {
                 adsenseActive ?
-                    (<Script
+                    <Script
                         strategy="afterInteractive"
                         async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3630578707238850"
                         crossOrigin="anonymous"
-                    />,
-                        console.log('Script AdSense cargado')
-                    )
+                    />
                     :
-                    (<Script
+                    <Script
                         strategy="beforeInteractive"
                         src={`https://www.ezojs.com/ezoic/sa.min.js`}
-                    />,
-                        console.log('Script Ezoic cargado')
-                    )
+                    />
             }
         </>
     )
